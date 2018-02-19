9fa6cdba0100e9c07ebd30ef5ba26c0045d103d2
changx03 <luke.x.chang@gmail.com>
2018-02-20 00:12:54 +1300

    added some es6 examples

--- es7.js
+++ es7.js
@@ -1,6 +1,31 @@
+// spread
 const a = [1, 2, 3];
 const b = [7,6, ...a];
 
+// rest operator
 const sort = (...args) => args.sort();
-
 console.log(sort(...b));
+
+const evenNum = (...args) => args.filter(i => i % 2 === 0);
+console.log(evenNum(...b));
+
+
+// destructuring
+[, w, exclamation] = ['Hello', 'world', '!'];
+console.log(w, exclamation);
+
+
+const { name, age: myAge } = { name: 'Luke Skywalker', age: '29' };
+console.log(name, myAge);
+
+
+// JS copy object's pointer
+// Deep copy example:
+const person = {
+  name: 'Max',
+};
+const person2 = person;
+const nextPerson = Object.assign({}, person);
+person.name = 'Maxx';
+console.log('person2', person2.name);
+console.log('nextPerson', nextPerson.name);
